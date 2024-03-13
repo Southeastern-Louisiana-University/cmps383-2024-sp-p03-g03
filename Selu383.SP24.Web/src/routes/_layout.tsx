7768a5bbe3b445e3af9fdf6e74c32dadfe38811b
Michael Rolling <michaelrolling2002@gmail.com>
2024-03-12 23:28:58 -0500

    Made cards to display hotel data (Still need to display the data from azure)

--- Selu383.SP24.Web/src/routes/_layout.tsx
+++ Selu383.SP24.Web/src/routes/_layout.tsx
@@ -2,6 +2,25 @@ import { useEffect, useState } from "react";
 import "./layout.css";
 import "../index.css";
 
+// Sample hotel data
+const hotelsData = [
+  {
+    id: 1,
+    name: "Holiday Inn",
+    roomTypes: ["1 bed 2 bath", "2 beds 2 bath"],
+  },
+  {
+    id: 2,
+    name: "Courtyard by Marriott",
+    roomTypes: ["1 bed 1 bath", "2 beds 1 bath"],
+  },
+  {
+    id: 3,
+    name: "Quality Inn",
+    roomTypes: ["1 bed 1 bath", "2 beds 1 bath"],
+  },
+];
+
 export default function HomePage() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -31,7 +50,6 @@ export default function HomePage() {
           </header>
         </div>
 
-        {/* Sidebar Content */}
         <div className={`sidebar ${isMenuOpen ? "sidebar-open" : ""}`}>
           <button className="close-btn" onClick={closeSidebar}>
             X
@@ -40,6 +58,23 @@ export default function HomePage() {
             <li>Login</li>
             <li>Sign Up</li>
           </ul>
+        </div>
+
+        <div className="body-content">
+          <div className="hotel-cards">
+            {hotelsData.map((hotel) => (
+              <div className="card" key={hotel.id}>
+                <h3>{hotel.name}</h3>
+                <p>Room Types Available:</p>
+                <ul>
+                  {hotel.roomTypes.map((type, index) => (
+                    <li key={index}>{type}</li>
+                  ))}
+                </ul>
+                <button className="book-now-btn">Book Now</button>
+              </div>
+            ))}
+          </div>
         </div>
 
         <div>
