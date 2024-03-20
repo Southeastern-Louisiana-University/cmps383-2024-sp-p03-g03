56fd28656073c4ce6d40982e314ae51835301a0e
Jonathan Williams <jonathan.williams-10@selu.edu>
2024-03-20 17:45:20 -0500

    Hotel Name in Rooms Controller

--- Selu383.SP24.Api/Features/Rooms/RoomDto.cs
+++ Selu383.SP24.Api/Features/Rooms/RoomDto.cs
@@ -6,8 +6,8 @@ namespace Selu383.SP24.Api.Features.Rooms
     {
         public int Id { get; set; }
         public int? HotelId { get; set; }
-        public string Beds { get; set; }
-
+        public string? Beds { get; set; }
+        public string? HotelName { get; set; }
         public bool IsAvailable { get; set; }
     }
 }
