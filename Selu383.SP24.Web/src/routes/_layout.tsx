8710b4faf2e3d9358df64547ab11717eaa451370
Michael Rolling <michaelrolling2002@gmail.com>
2024-04-03 02:52:48 -0500

    Implementing MUI material to home page and hotel list

--- Selu383.SP24.Web/src/routes/_layout.tsx
+++ Selu383.SP24.Web/src/routes/_layout.tsx
@@ -5,10 +5,22 @@ import { useNavigate } from "react-router-dom";
 import useFetch from "use-http";
 import AuthContext from "../features/authentication/AuthContext";
 import UserDto from "../features/authentication/UserDto";
+import { Box, Modal, Button, IconButton, AppBar, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function MainLayout() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [currentUser, setCurrentUser] = useState<null | undefined | UserDto>(undefined);
+  const navigate = useNavigate();
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    console.log("layout loaded");
+  }, []);
 
   useFetch(
     "api/authentication/me",
@@ -25,66 +37,51 @@ export default function MainLayout() {
     []
   );
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
   };
-
-  const closeSidebar = () => {
-    setIsMenuOpen(false);
-  };
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    console.log("layout loaded");
-  }, []);
 
   return (
     <>
       <AuthContext.Provider value={{ user: currentUser, setUser: setCurrentUser }}></AuthContext.Provider>
-      <div className={`body ${isMenuOpen ? "menu-open" : ""}`}>
-        <div className="container">
-          <header className="header">
-            <div className="logo">EnStay</div>
-            <button className="burger-menu" onClick={toggleMenu}>
-              <div className="line"></div>
-              <div className="line"></div>
-              <div className="line"></div>
-            </button>
-          </header>
-        </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar color="success" position="static">
+          <Toolbar sx={{ padding: 1 }}>
+            <IconButton size="large" edge="start" color="inherit" aria-label="menu">
+              <MenuIcon fontSize="inherit" />
+            </IconButton>
+            <Typography align="center" variant="h6" component="div" sx={{ flexGrow: 1, fontSize: 30 }}>
+              EnStay
+            </Typography>
+            <IconButton onClick={handleOpen} size="large" edge="start" color="inherit" aria-label="menu">
+              <AccountCircleIcon fontSize="inherit" />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
+          <Button onClick={() => navigate("/login")}>Login</Button>
+          <Button onClick={() => navigate("/signup")}>Sign up</Button>
+        </Box>
+      </Modal>
 
-        <div className={`sidebar ${isMenuOpen ? "sidebar-open" : ""}`}>
-          <button className="close-btn" onClick={closeSidebar}>
-            X
-          </button>
-          <ul>
-            <button className="button" onClick={() => navigate("/login")}>
-              Login
-            </button>
-            <br />
-            <br />
-            <button className="button" onClick={() => navigate("/signup")}>
-              Sign Up
-            </button>
-          </ul>
-        </div>
-
-        <div className="body-content">
-          <label className="location-label" htmlFor="search">
-            Start by finding hotels!
-          </label>
-          <br />
-          <button className="button" onClick={() => navigate("/hotels")}>
-            List Hotels
-          </button>
-        </div>
-
-        <footer className="footer">
-          <div className="footer-content">
-            <p>&copy; 2024 EnStay. All Rights Reserved.</p>
-          </div>
-        </footer>
+      <div className="body-content">
+        <label className="location-label" htmlFor="search">
+          Start by finding hotels!
+        </label>
+        <br />
+        <Button className="button" onClick={() => navigate("/hotels")} variant="contained" color="success">
+          List Hotels
+        </Button>
       </div>
     </>
   );
--- Selu383.SP24.Web/src/routes/hotel-pages/list-hotels.tsx
+++ Selu383.SP24.Web/src/routes/hotel-pages/list-hotels.tsx
@@ -2,11 +2,14 @@
 import { useEffect } from "react";
 import { useFetch } from "use-http";
 import { HotelDto } from "../../features/hotel-dtos/HotelDto"; // Import the HotelDto interface
-import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import { AppBar, Button, Card, CardActionArea, CardContent, CardMedia, IconButton, Toolbar, Typography } from "@mui/material";
 import "../layout.css";
+import { useNavigate } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const ListHotels = () => {
   const { data: hotels, loading, error, get } = useFetch<HotelDto[]>("/api/hotels");
+  const navigate = useNavigate();
 
   useEffect(() => {
     get();
@@ -22,16 +25,26 @@ const ListHotels = () => {
 
   return (
     <div>
-      <h1>Hotel List</h1>
-      <a href="/" className="close-btn">
-        Back Home
-      </a>
+      <AppBar color="success" position="static">
+        <Toolbar sx={{ padding: 1 }}>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu">
+            <MenuIcon fontSize="inherit" />
+          </IconButton>
+          <Typography align="center" variant="h6" component="div" sx={{ flexGrow: 1, fontSize: 30 }}>
+            Hotels
+          </Typography>
+          <Button onClick={() => navigate("/")}>Back Home</Button>
+        </Toolbar>
+      </AppBar>
+
       <div className="hotel-list">
         {hotels &&
           hotels.map((hotel: HotelDto) => (
-            <CardActionArea>
-              <Card variant="outlined" key={hotel.id} className="hotel-card">
-                <img src="https://media.istockphoto.com/id/1084106348/photo/apartment-home-residential-building-complex-street-parking.jpg?s=612x612&w=0&k=20&c=CO0zL7cmvkw1r4mvEc0JTC9s2uyhYV9pOhuGuZF9NHU=" className="card-image" />
+            <Card className="hotel-card" sx={{ maxWidth: 400 }}>
+              <CardActionArea key={hotel.id}>
+                <CardMedia>
+                  <img src="https://media.istockphoto.com/id/1084106348/photo/apartment-home-residential-building-complex-street-parking.jpg?s=612x612&w=0&k=20&c=CO0zL7cmvkw1r4mvEc0JTC9s2uyhYV9pOhuGuZF9NHU=" className="card-image" />
+                </CardMedia>
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
                     {hotel.name}
@@ -41,8 +54,8 @@ const ListHotels = () => {
                   </Typography>
                   {/* Add more hotel details as needed */}
                 </CardContent>
-              </Card>
-            </CardActionArea>
+              </CardActionArea>
+            </Card>
           ))}
       </div>
     </div>
